9895fc17a8136979fe4c3cbf5875feb915360812
Ogarri <143884627+Ogarri@users.noreply.github.com>
2025-04-09 15:25:58 +0200

    Mise a jour du TasMin

--- TasMin.py
+++ TasMin.py
@@ -1,57 +1,45 @@
 class TasMin:
-    def __init__(self):
-        # Initialisation du tas sous forme de liste
-        self.tas = []
+    def __init__(self, tas=[]):
+        self.tas = tas
+        self.nTas = len(tas)
 
-    def ajouter(self, element):
-        # Ajout d'un élément au tas et réorganisation
-        self.tas.append(element)
-        self._tamiser_haut(len(self.tas) - 1)
+    def maximum(self):
+        # Retourne le maximum du tas (racine pour un tas max)
+        if self.nTas == 0:
+            return None
+        return self.tas[0]
+
+    def ajouter(self, v):
+        i = self.nTas
+        self.tas.append(v)
+        self.nTas += 1
+        while i > 0 and self.tas[(i - 1) // 2] <= v:
+            self.tas[i] = self.tas[(i - 1) // 2]
+            i = (i - 1) // 2
+        self.tas[i] = v
 
     def retirer(self):
-        # Retrait de l'élément minimum (racine) du tas
-        if not self.tas:
+        if self.nTas == 0:
             return None
-        self._echanger(0, len(self.tas) - 1)
-        element_min = self.tas.pop()
-        self._tamiser_bas(0)
-        return element_min
-
-    def _tamiser_haut(self, index):
-        # Réorganisation du tas après ajout d'un élément
-        parent = (index - 1) // 2
-        while index > 0 and self.tas[index][0] < self.tas[parent][0]:
-            self._echanger(index, parent)
-            index = parent
-            parent = (index - 1) // 2
-
-    def _tamiser_bas(self, index):
-        # Réorganisation du tas après retrait de l'élément minimum
-        enfant = 2 * index + 1
-        while enfant < len(self.tas):
-            droit = enfant + 1
-            if droit < len(self.tas) and self.tas[droit][0] < self.tas[enfant][0]:
-                enfant = droit
-            if self.tas[index][0] <= self.tas[enfant][0]:
+        # Suppression de l'élément minimum (racine du tas)
+        minimum = self.tas[0]
+        self.tas[0] = self.tas[self.nTas - 1]
+        self.nTas -= 1
+        del self.tas[self.nTas]
+        if self.nTas == 0:
+            return minimum
+        # Réorganiser le tas pour maintenir la propriété
+        i = 0
+        while 2 * i + 1 < self.nTas:
+            j = 2 * i + 1
+            if j + 1 < self.nTas and self.tas[j + 1] < self.tas[j]:
+                j += 1
+            if self.tas[i] <= self.tas[j]:
                 break
-            self._echanger(index, enfant)
-            index = enfant
-            enfant = 2 * index + 1
-
-    def _echanger(self, i, j):
-        # Échange de deux éléments dans le tas
-        self.tas[i], self.tas[j] = self.tas[j], self.tas[i]
+            self.tas[i], self.tas[j] = self.tas[j], self.tas[i]
+            i = j
+        return minimum
 
     def est_vide(self):
         # Vérifie si le tas est vide
-        return len(self.tas) == 0
-
-    def mettre_a_jour(self, element):
-        # Met à jour un élément dans le tas
-        for i, (valeur, sommet) in enumerate(self.tas):
-            if sommet == element[1]:
-                self.tas[i] = element
-                self._tamiser_haut(i)
-                self._tamiser_bas(i)
-                break
-    
+        return self.nTas == 0
